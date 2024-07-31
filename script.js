88865c6300772dc1cd4d268cbdbcc9e11fc806e1
isa <tagirovisa227>
2024-07-31 15:35:38 +0300

    function

--- script.js
+++ script.js
@@ -1,95 +1,56 @@
+let num = 20;
 
-// Программа для рассчета финансовых 
-// показателей на JavaScript
-
-let money = +prompt('Какая у вас зарплата на месяц?');
-
-time = prompt('Введите дату в формате YYYY-MM-DD');
-
-let appDate = {
-  budget: money,
-  timeDate: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  savings: false
+function showFirstMassege(text) {
+  alert(text);
+  let num = 10;
+  console.log(num);
 }
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-      b = prompt('Во сколько обойдется?', '');
+showFirstMassege('Привет!');
 
-  if (typeof(a) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-      && a != '' && b != '' && a.length < 10) {
-
-    console.log('done');
-
-    appDate.expenses[a] = b;
-
-  } else {
-     console.log('Ошибка!');
-     i--;
-  }
-};
-
-// let i = 0;
-
-// while (i < 2) {
-//   let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-//       b = prompt('Во сколько обойдется?', '');
-
-//   if (typeof(a) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-//       && a != '' && b != '' && a.length < 10) {
-
-//     console.log('done');
-
-//     appDate.expenses[a] = b;
-
-//   } else {
-//      console.log('Ошибка!');
-//      i--;
-//   }
-//   i++;
-// }
+console.log(num);
 
 
-// let i = 0; 
-
-// do {
-//   let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-//       b = prompt('Во сколько обойдется?', '');
-
-//   if (typeof(a) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-//       && a != '' && b != '' && a.length < 10) {
-
-//     console.log('done');
-
-//     appDate.expenses[a] = b;
-
-//   } else {
-//      console.log('Ошибка!');
-//      i--;
-//   }
-//   i++;
-// }
-// while (i < 2);
-
-
-appDate.moneyPerDay = appDate.budget / 30;
-
-alert('Ежедневный бюджет: ' + appDate.moneyPerDay);
-
-if (appDate.moneyPerDay < 2000) {
-  console.log('Минимальная зарплата');
-} else if (appDate.moneyPerDay > 2000 && appDate.moneyPerDay < 3000) {
-  console.log('Средняя зарплата');
-} else if (appDate.moneyPerDay > 3000) {
-  console.log('Высокая зарплата');
-} else {
-    console.log('Ошибка');
+function calculate(a,b) {
+  return (a + b);
 }
 
-alert('Бюджет на один день составляет: ' + appDate.moneyPerDay + 'руб.');
+console.log(calculate(3,4));
 
-console.log(appDate);
+console.log(calculate(8,4));
 
+function retVar() {
+  let num = 30;
+  return num;
+}
+
+let a = retVar();
+
+console.log(a);
+
+
+let calc = function(a,b) {
+  return (a + b);
+}
+
+// let calc = (a, b) => {a+b} 
+
+
+// function declaration & expation
+
+
+let str = 'test';
+
+console.log(str.length);
+
+console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
+
+let number = '30.7px';
+
+console.log(Math.round(number));
+
+console.log(parseInt(number));
+
+console.log(parseFloat(number));
