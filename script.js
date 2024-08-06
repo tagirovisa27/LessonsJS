1802c4940d8f1b64eed514de92bbef278748c83d
isa <tagirovisa227>
2024-08-06 16:45:55 +0300

    objects

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let divClassSection = {
+  width: 100,
+  height: 50,
+  border: 10,
+  color: 'black'
+};
 
-function learnJs(lang, callback) {
-   console.log('Я учу '+ lang);
-   callback();
+console.log(divClassSection['width']);
+console.log(divClassSection.height);
+
+divClassSection.bool = true;
+divClassSection.title = 'text';
+divClassSection.classNav = {
+    list: 5,
+    links: true
+};
+
+delete divClassSection.color;
+
+console.log(divClassSection);
+
+
+for (let key in divClassSection) {
+    console.log('Свойство ' + key + ' имеет значение ' + divClassSection[key]);
 }
 
-learnJs('JavaScript', done);
-
-function done() {
-  console.log('Я прошел 3-й урок!');
-}
-
+console.log(Object.keys(divClassSection).length);
