f0fd8771712c50ab371451d5cefe147cbff85955
isa <tagirovisa227>
2024-08-06 14:11:05 +0300

    callback function

--- script.js
+++ script.js
@@ -1,77 +1,12 @@
 
-// Программа для рассчета финансовых 
-// показателей на JavaScript
-
-let money, time;
-
-function start() {  
-    money = +prompt('Какая у вас зарплата на месяц?', '');
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
-
-    while (isNaN(money) || money == '' || money == null) {
-        money = +prompt('Какая у вас зарплата на месяц?', '');  
-    }
-}
-start();  
-
-let appDate = {
-  budget: money,
-  timeDate: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  savings: true
+function learnJs(lang, callback) {
+   console.log('Я учу '+ lang);
+   callback();
 }
 
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt('Введите обязательную статью расходов в этом месяце', '');
-        b = +prompt('Во сколько обойдется?', '');
-  
-      if (typeof (a) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-         && a != '' && b != '' && a.length < 10) {
-         appDate.expenses[a] = b;
-    } else {
-      i--;
-    }
-  }  
-}
-chooseExpenses();
+learnJs('JavaScript', done);
 
-function detectDayBudget() {
-  appDate.moneyPerDay = (appDate.budget / 30).toFixed();
-    alert('Ежедневный бюджет: ' + appDate.moneyPerDay + 'руб.');
+function done() {
+  console.log('Я прошел 3-й урок!');
 }
-detectDayBudget();
 
-function detectLevel() {
-  if (appDate.moneyPerDay < 2000) {
-    alert('Минимальная зарплата');
-  } else if (appDate.moneyPerDay > 2000 && appDate.moneyPerDay < 3000) {
-    alert('Средняя зарплата');
-  } else if (appDate.moneyPerDay > 3000) {
-    alert('Высокая зарплата');
-  } else {
-    alert('Ошибка!');
-  }
-}
-detectLevel();
-
-function checkSavings() {
-   if (appDate.savings == true) {
-      let save = +prompt('Какова сумма накоплений?'),
-        perсent = +prompt('Под какой процент?');
-
-        appDate.monthIncome = (save/100/12*perсent).toFixed();
-          alert('Доход в месяц с вашего депозита: ' + appDate.monthIncome);  
-   }  
-}
-checkSavings();
-
-function chooseOptExpenses() {
-  for (let i = 0; i < 3; i++) {
-     let questionOptExpenses = prompt('Статья необязательных расходов?');
-     appDate.optionalExpenses[i] = questionOptExpenses;
-  } 
-}
-chooseOptExpenses();
