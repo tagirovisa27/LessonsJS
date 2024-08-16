1c4befb545ea8f542aa7c57c9fb5672256d2d237
isa <tagirovisa227>
2024-08-16 19:53:59 +0300

    lesson 4

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,6 +1,29 @@
+let startBtn = document.getElementById('start'),
 
-// Программа для рассчета финансовых 
-// показателей на JavaScript
+  budgetValue = document.getElementsByClassName('budget-value')[0],
+  dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+  levelValue = document.getElementsByClassName('level-value')[0],
+  expensesValue = document.getElementsByClassName('expenses-value')[0],
+  optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+  incomeValue = document.getElementsByClassName('income-value')[0],
+  monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+  yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+  expensesItem = document.getElementsByClassName('expenses-item')[0],
+
+  appOffBtn = document.getElementsByTagName('button')[0],
+  appOnBtn = document.getElementsByTagName('button')[1],
+  calcBtn = document.getElementsByTagName('button')[2],
+
+  optionalexpensesItem = document.querySelectorAll('optionalexpenses-item'),
+
+  incomeItem = document.querySelector('.choose-income'),
+  savingsBtn = document.querySelector('#savings'),
+  Sum = document.querySelector('.choose-sum'),
+  percent = document.querySelector('.choose-percent'),
+  yearValue = document.querySelector('.year-value'),
+  monthValue = document.querySelector('.month-value'),
+  dayValue = document.querySelector('.day-value');
 
 let money, time;
 
