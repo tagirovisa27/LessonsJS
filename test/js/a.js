73f1d1b09956f022e1daef733f10eaf6050790ed
isa <tagirovisa227>
2024-09-07 17:33:11 +0300

    AJAX

---
+++ test/js/a.js
@@ -0,0 +1,41 @@
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'js/a.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', function () {
+        if (request.readyState === 4 && request.status == 200) {
+            let date = JSON.parse(request.response);
+
+            inputUsd.value = inputRub.value / date.usd;
+        } else {
+            inputUsd.value = "Ошибка!";
+        }
+
+    });
+});
+
+// AJAX - 
+
+// http-запросы
+// XMLHttpRequest(); - Объект
+
+// Методы 
+// open - Настройка запроса
+// method - общение между клиентом и сервером GET POST
+// URL - Путь к серверу локальный глобальный итд  
+// async - Асинхронность запроса по умолчанию true (false)
+// login password - Имя пользователя и пороль
+
+
+// Свойство
+// status - http-код ответ от сервера - 404, 403, 0 итд
+// statusText - Текстовый ответ от сервера - ok, notfound
+// responseText - Текст ответа сервера от разработчика  - response
+// readyState - Текущая тояние запроса - этапы 
+
