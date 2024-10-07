c236d87c324e2380642924600d7fc9412939a972
isa <tagirovisa227>
2024-10-07 16:20:21 +0300

    Encapsulation & Module

--- test/js/a.js
+++ test/js/a.js
@@ -1,19 +1,83 @@
-// SPA - Single Page Application - Одностраничное приложение
+// // Инкапсуляция
 
-// Библиотеки и фреймаворки
+// function User(name, age) {
+//     this.name = name;
+//     // this.age = age;
+//     let userAge = age;
 
-// 1 node.js - npm
-// 2 TypeScript - ЯП
-// 3 Webpack - Сборщик файлов
-// 4 MVC - (modal view controler)
-// 5 Angular 
+//     this.say = function () {
+//         console.log(`Имя пользователя ${this.name}, возраст ${userAge}`);
+//     }
 
-// 1 node.js - npm
-// 2 Babel - software
-// 3 JSX - Препроцессор
-// 4 Webpack - Сборщик файлов
-// React
+//     this.getAge = function () {
+//         return userAge;
+//     }
 
-// 1 node.js - npm
-// 2 Babel - software
-// 3 Webpack - Сборщик файлов
+//     this.setAge = function (age) {
+//         if (typeof age === 'number' && age > 0 && age < 30) {
+//             userAge = age;
+//         } else {
+//             console.log('Ошибка!');
+//         }
+//     }
+// }
+
+// let Isa = new User('Isa', 23);
+// console.log(Isa.name);
+// console.log(Isa.userAge);
+
+// console.log(Isa.getAge());
+// Isa.setAge(24);
+// console.log(Isa.getAge());
+
+// Isa.say();
+
+// Анонимная самовызовающая функция (Модули)
+
+let number = 1;
+
+(function () {
+    let number = 2;
+    console.log(number);
+
+    return console.log(number + 3);
+}());
+
+console.log(number);
+
+// Использование объектного интерфейса
+
+let user = (function () {
+    let privat = function () {
+        console.log('I ma privat');
+    }
+
+    return {
+        sayHello: function () {
+            console.log('Hello!');
+        }
+    }
+}())
+
+console.log(user);
+console.log(user.sayHello());
+
+
+// Все созданные методы скрыты снаружи 
+
+let users = (function () {
+    let privat = function () {
+        console.log('I ma privat');
+    }
+
+    let sayHello = function () {
+        console.log('Hello!');
+    }
+
+    return {
+        sayHello: sayHello
+    }
+}())
+
+console.log(user);
+console.log(user.sayHello());
